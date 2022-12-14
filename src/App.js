ea4f4728913de517d8b59703e065590fe2d5e883
Morgan Rector <mmrector@yahoo.com>
2022-12-14 14:38:07 -0800

    Phase two completed, coffee bean name updating each render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
+import SetCoffeeBean from "./components/SetCoffeeBean";
 import coffeeBeans from "./mockData/coffeeBeans.json";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <h1>Welcome to Coffee App</h1>
       <SelectedCoffeeBean />
+      <SetCoffeeBean coffeeBeans={coffeeBeans}/>
     </>
   );
 }
--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,10 +1,25 @@
-const SetCoffeeBean = ({ coffeeBeans }) => {
+import { useEffect, useState } from "react";
+import { useCoffeeContext } from "../context/CoffeeContext";
+
+const SetCoffeeBean = ({ coffeeBeans}) => {
+
+  const {coffeeBean, setCoffeeBeanId} = useCoffeeContext();
+    // console.log(setCoffeeBeanId)
+
+  const handleClick = (e)=> {
+    console.log('inside event listener')
+    console.log(e.target.value)
+    e.preventDefault();
+    setCoffeeBeanId(e.target.value)
+  }
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
-      >
+        value={coffeeBean.id}
+        onChange={handleClick}>
         {coffeeBeans.map(bean => (
           <option
             key={bean.id}
