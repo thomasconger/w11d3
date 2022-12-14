e3b380cf98614d4e3ccdfcd63bfd09084352b927
Morgan Rector <mmrector@yahoo.com>
2022-12-14 15:37:59 -0800

    Light switch added

--- greenhouse/src/components/Greenhouse/LightSwitch.js
+++ greenhouse/src/components/Greenhouse/LightSwitch.js
@@ -1,8 +1,22 @@
+import { useTheme } from '../../context/ThemeContext';
 import './LightSwitch.css';
 
+
+
 function LightSwitch() {
+  const {themeName, setThemeName} = useTheme()
+
+  const handleClick = (e)=>{
+    if(e.target.className === 'on'){
+      setThemeName('day')
+    }else{
+      setThemeName('night')
+    }
+  }
+
+
   return (
-    <div className="light-switch day">
+    <div className={`light-switch ${themeName}`} onClick={handleClick}>
       <div className="on">DAY</div>
       <div className="off">NIGHT</div>
     </div>
